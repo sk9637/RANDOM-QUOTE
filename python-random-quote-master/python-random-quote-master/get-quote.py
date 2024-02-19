8c358e1bc378031f3c17c4b17375d253cb233c13
sk9637 <103405761+sk9637@users.noreply.github.com>
2024-02-20 00:08:33 +0530

    Add files via upload

---
+++ python-random-quote-master/python-random-quote-master/get-quote.py
@@ -0,0 +1,19 @@
+import random
+
+last = 13
+rnd = random.randint(0, last)
+
+
+def primary():
+    print("Keep it logically awesome.")
+    print("Keep it logically awesome.")
+
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+
+    print(quotes[rnd])
+
+
+if __name__ == "__main__":
+    primary()
